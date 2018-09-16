55d6699b9057187c8c2ed4f2d3ac6266db370cf3
Caio Gobbi <caiofelipegobbi@gmail.com>
2018-09-16 20:13:48 -0300

    aula 06

---
+++ Aula006a.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+
+#print('Resultado: ', soma)
+
+print('A Soma entre {} e {} é {}'.format(n1, n2, soma))
